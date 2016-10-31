099d9afa48c974ccf3081385c321252ead7fe6cf
Johnny <Johnny@LAPTOP-MB4CFRJQ>
2016-10-31 18:50:30 +0100

    Revert "Revert "Fixed so the Fileupload now saves the Filepath and userid to DB""
    This reverts commit da916c572f23285f3fc112bd66321bc1e4705517.

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -5,14 +5,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.Script.Serialization;
+using PictureGalleryProject.Models;
 
 namespace PictureGalleryProject.Controllers
 {
     public class FilesController : Controller
     {
+
+        private PictureGalleryModel db = new PictureGalleryModel();
         // GET: Files
         // Whenever the application loads, the action result will be fired
-       
+        [Authorize]
         public ActionResult Index()
         {
             foreach (string upload in Request.Files)
@@ -21,14 +26,37 @@ namespace PictureGalleryProject.Controllers
                 // App_Data / Uploaded_Files /
                 if (Request.Files[upload].FileName != "")
                 {
-                    string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/Uploaded_Files/";
-                    string filename = Path.GetFileName(Request.Files[upload].FileName);
-                    Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                    //Does another check to see if the user is logged in
+                    //If logged in -> upload file -> Get UserId -> Get filepath -> Save logged in UserID and uploaded file's path to database
+                    if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+                    {
+                        //Uploading file to App_Data/Uploaded_Files
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/Uploaded_Files/";
+                        string filename = Path.GetFileName(Request.Files[upload].FileName);
+                        Request.Files[upload].SaveAs(Path.Combine(path, filename));
+
+                        //Getting Logged in users UserID.
+                        var id = System.Web.HttpContext.Current.User.Identity.Name;
+                        var intID = Int32.Parse(id); //Parse it to be able to add it into the database, This will always be a number
+
+                        //Getting filepath
+                        string fl = path.Substring(path.LastIndexOf("\\"));
+                        string[] split = fl.Split('\\');
+                        string newpath = split[1];
+                        string FileURL = newpath + filename;
+
+                        //Save UserID and Path to database
+                        PictureInfo NewFile = new PictureInfo();
+                        NewFile.UserID = intID;
+                        NewFile.PictureURI = FileURL;
+                        db.PictureInfoes.Add(NewFile);
+                        db.SaveChanges();
+                    }
                 }
             }
             return View("Upload");
         }
-
+        [Authorize]
         public ActionResult Downloads()
         {
             // Getting all file information from App_Data / Uploaded_Files 
