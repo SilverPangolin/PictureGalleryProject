b2a2806c8ad7d23b2bda952cb0b812595be4edd5
Johnny <Johnny@LAPTOP-MB4CFRJQ>
2016-11-02 20:20:42 +0100

    Added WCF service(Needs more work) added windows form

---
+++ FileService.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PictureGalleryProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "FileService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select FileService.svc or FileService.svc.cs at the Solution Explorer and start debugging.
+    public class FileService : IFileService
+    {
+        public string GetFiles(getfiles filesInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsers(getusers userInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetFiles(setfiles filesInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetUsers(setusers userInfo)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection(@"data source=(LocalDB)\MSSQLLocalDB;attachdbfilename=|DataDirectory|\Projectdb.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Users(UserName,Password,FirstName,LastName) values(@UserName,@Password,@Country,@Email)", con);
+            cmd.Parameters.AddWithValue("@UserName", userInfo.UserName);
+            cmd.Parameters.AddWithValue("@Password", userInfo.Password);
+            cmd.Parameters.AddWithValue("@Country", userInfo.FirstName);
+            cmd.Parameters.AddWithValue("@Email", userInfo.LastName);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = userInfo.UserName + " Details inserted successfully";
+            }
+            else
+            {
+                Message = userInfo.UserName + " Details not inserted successfully";
+            }
+            con.Close();
+            return Message;
+        }
+    }
+}
---
+++ IFileService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PictureGalleryProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IFileService" in both code and config file together.
+    [ServiceContract]
+    public interface IFileService
+    {
+        [OperationContract]
+        string GetUsers(getusers userInfo);
+
+        [OperationContract]
+        string SetUsers(setusers userInfo);
+
+        [OperationContract]
+        string GetFiles(getfiles filesInfo);
+
+        [OperationContract]
+        string SetFiles(setfiles filesInfo);
+    };
+
+    public class getusers
+    {
+        string username = string.Empty;
+        string password = string.Empty;
+        string firstname = string.Empty;
+        string lastname = string.Empty;
+
+        [DataMember]
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        [DataMember]
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+
+    public class setusers
+    {
+        string username = string.Empty;
+        string password = string.Empty;
+        string firstname = string.Empty;
+        string lastname = string.Empty;
+
+        [DataMember]
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        [DataMember]
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+        [DataMember]
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+    }
+
+    public class getfiles
+    {
+        string fileurl = string.Empty;
+        string userid = string.Empty;
+
+        [DataMember]
+        public string FileURL
+        {
+            get { return fileurl; }
+            set { fileurl = value; }
+        }
+
+        [DataMember]
+        public string UserID
+        {
+            get { return userid; }
+            set { userid = value; }
+        }
+    }
+
+    public class setfiles
+    {
+        string fileurl = string.Empty;
+        string userid = string.Empty;
+
+        [DataMember]
+        public string FileURL
+        {
+            get { return fileurl; }
+            set { fileurl = value; }
+        }
+
+        [DataMember]
+        public string UserID
+        {
+            get { return userid; }
+            set { userid = value; }
+        }
+    }
+}
